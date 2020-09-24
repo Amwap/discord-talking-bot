566a6ecc1e00b12cc7c57157bc83db7e3b437bf1
Илья Коновалов <Amwap>
2020-09-24 15:44:42 +0300

    answer/learn is working

---
+++ bot.py
@@ -0,0 +1,62 @@
+import discord
+from dora_client import Dora_client
+
+TOKEN = 'NzQ1NjY4ODc3NzA2NDYxMzQ3.Xz1IQg.WY9_WQMwN0svQdXM9gkykDsUr4E'
+client = discord.Client()
+bot = Dora_client()
+
+
+bot.key = 'lvklvsdN'
+last_message = {}
+rating_stop = {}
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    text = message.content.replace('d!', '')
+    lowtext = text.lower().strip()
+    if message.author == client.user:
+        return
+
+    else:
+        if lowtext.startswith('add'):
+            try:
+                arg1, arg2 = text.replace('add', '').split('=')
+                author = f'discord {message.author.id}'
+                response = bot.learn(arg1, arg2, author)
+                answer = response['system_message']
+
+            except IndexError:
+                answer = 'Ошибочная команда. попробуй "d! add вопрос = ответ"'
+
+        elif lowtext.startswith('r+') or lowtext.startswith('r-'):
+            if rating_stop[message.author] == last_message[message.channel.id]:
+                answer = 'Больше одного раза голосовать нельзя.'
+
+            else:
+                if lowtext.startswith('r+'): operator = 'rup'
+                if lowtext.startswith('r-'): operator = 'rdown'
+                response = bot.rating(operator, last_message[message.channel.id])
+                answer = response['system_message']
+                rating_stop[message.author] = last_message[message.channel.id]
+                print(message.author, response)
+
+
+
+        else:
+            response = bot.answer(text)
+            last_message[message.channel.id] = response['response_id']
+            rating_stop[message.author] = -1
+            answer = f"{response['answer']} ({response['coefficient']})"
+            print(message.author, response)
+
+
+        await message.channel.send(answer)
+
+
+client.run(TOKEN)
